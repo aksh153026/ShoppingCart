0fc6e55102ee120b5e25bf0084868a44c0ca1b6e
aksh153026 <57639109+aksh153026@users.noreply.github.com>
2021-05-06 17:25:54 +0530

    Update ProductService.java

--- src/main/java/com/service/ProductService.java
+++ src/main/java/com/service/ProductService.java
@@ -4,14 +4,9 @@ import java.util.List;
 
 import com.model.Product;
 public interface ProductService {
-
 	List<Product> getAllProducts();
-
 	Product getProductById(String productId);
-
 	void deleteProduct(String productId);
-	
 	void addProduct(Product product);
-	
 	void editProduct(Product product);
 }
